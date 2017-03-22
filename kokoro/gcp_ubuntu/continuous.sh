7c0c361267ebc14f17eb6aff111be10e6e3caf6e
Andras Kerekes <andras.kerekes.work@gmail.com>
2017-03-22 14:31:05 -0400

    added missing cd

--- kokoro/gcp_ubuntu/continuous.sh
+++ kokoro/gcp_ubuntu/continuous.sh
@@ -6,6 +6,7 @@ set -e
 set -x
 
 git clone https://github.com/leachim6/hello-world.git
+cd hello-world
 # Checkout the commit we trust
 git checkout 3bab02464b0fdc7c0e59cd39744ea432ec2baafa
 cd hello-world/j
